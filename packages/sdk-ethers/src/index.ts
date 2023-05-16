a0052a2e63666d3f34608a86b618cc82d684cbb2
Johnny Shankman <johnnyshankman@users.noreply.github.com>
2023-05-15 23:01:48 -0400

    make getAccount algorithmic and non-async, no provider needed

--- packages/sdk-ethers/src/index.ts
+++ packages/sdk-ethers/src/index.ts
@@ -1,4 +1,4 @@
-import { Contract, Signer, providers, utils, BigNumberish } from "ethers";
+import { Contract, Signer, utils, BigNumberish, BigNumber } from "ethers";
 import erc6551RegistryAbi from "../abis/ERC6551Registry.json";
 import erc6551AccountAbi from "../abis/IERC6551Account.json";
 
@@ -10,26 +10,44 @@ export const erc6551RegistryAddress =
 export const erc6551AccountImplementationAddress =
   "0x36963236d915e4e9b5f70677eBD1ea3e69Cfbbd6" as const;
 
-export async function getAccount(
+export function getAccount(
+  chainId: number,
   tokenContract: string,
   tokenId: string,
-  provider: providers.BaseProvider
-) {
-  const registry = new Contract(
-    erc6551RegistryAddress,
-    erc6551RegistryAbi,
-    provider
-  );
-
-  const { chainId } = await provider.getNetwork();
-
-  return registry.callStatic.account(
+): string {
+  const code = getCreationCode(
     erc6551AccountImplementationAddress,
     chainId,
     tokenContract,
     tokenId,
-    0
+    "0"
   );
+  const codeHash = utils.keccak256(code);
+  const saltHex = utils.hexZeroPad(BigNumber.from(0).toHexString(), 32);
+  return utils.getCreate2Address(
+    erc6551RegistryAddress,
+    saltHex,
+    codeHash,
+  );
+}
+
+export function getCreationCode(
+  implementation_: string,
+  chainId_: number,
+  tokenContract_: string,
+  tokenId_: string,
+  salt_: string,
+): Uint8Array {
+  const types = ["uint256", "uint256", "address", "uint256"];
+  const values = [salt_, chainId_, tokenContract_, tokenId_];
+  const creationCode = utils.concat([
+    "0x3d60ad80600a3d3981f3363d3d373d3d3d363d73",
+    utils.hexlify(implementation_),
+    "0x5af43d82803e903d91602b57fd5bf3",
+    utils.defaultAbiCoder.encode(types, values)
+  ]);
+
+  return creationCode;
 }
 
 export async function createAccount(
