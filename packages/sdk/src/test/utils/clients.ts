aa5973edd1335d23072043c2997d59447742d558
BJ Vicks <bj@bjvicks.com>
2023-10-03 13:39:26 -0700

    fix issue where test files were causing build to fail

--- packages/sdk/src/test/utils/clients.ts
+++ packages/sdk/src/test/utils/clients.ts
@@ -1,4 +1,10 @@
-import { createPublicClient, WalletClient, PublicClient, createWalletClient, http } from 'viem'
+import {
+  createPublicClient,
+  WalletClient,
+  PublicClient,
+  createWalletClient,
+  http,
+} from 'viem'
 import { foundry } from 'viem/chains'
 import { chainIdToChain } from '../../utils'
 import { ANVIL_RPC_URL, ANVIL_ACCOUNTS } from '../constants'
@@ -10,14 +16,13 @@ export const getMockWalletClient = () =>
     account: ANVIL_ACCOUNTS[0].address as `0x${string}`,
     key: ANVIL_ACCOUNTS[0].privateKey,
     pollingInterval: 100,
-}) as WalletClient
+  }) as WalletClient
 
 export const getPublicClient = ({
   chainId = foundry.id,
 }: {
   chainId?: number
 }): PublicClient => {
-
   const chain = chainIdToChain(chainId)
 
   if (!chain) throw new Error(`Chain ${chainId} not found`)
