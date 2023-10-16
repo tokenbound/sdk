d7def90f3bf7765d5295936d742cf688516fdc8b
BJ Vicks <bj@bjvicks.com>
2023-10-16 11:00:04 -0700

    add opgoerli to internal chains

--- packages/sdk/src/utils/chainIdToChain.ts
+++ packages/sdk/src/utils/chainIdToChain.ts
@@ -7,6 +7,7 @@ import {
   sepolia,
   optimism,
   arbitrum,
+  optimismGoerli,
   base,
   gnosis,
 } from 'viem/chains'
@@ -14,6 +15,7 @@ import {
 const allChains = {
   mainnet,
   goerli,
+  optimismGoerli,
   polygon,
   polygonMumbai,
   sepolia,
