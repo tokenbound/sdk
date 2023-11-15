367b6ec7bff7d8e04b7410774f2d0e76ec601d49
Ray <huynh.r.k@gmail.com>
2023-11-14 17:30:43 -0800

    allow for mumbai tx with 4337

--- packages/sdk/src/constants/bundlerChains.ts
+++ packages/sdk/src/constants/bundlerChains.ts
@@ -5,7 +5,7 @@ type bundlerChains = {
 export const bundlerChains: bundlerChains = {
   1: true, //mainnet
   5: true, //goerli
-  80001: false, //mumbai
+  80001: true, //mumbai
   137: true, //polygon
   10: false, //optimism
   420: false, //op goerli
