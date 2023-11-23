f4b9ef684fa1eb1bc9439e28c0ca7d0debf9df1c
Jayden Windle <jaydenwindle@gmail.com>
2023-11-22 17:22:51 -0800

    Parameterized tests to allow for easier isolation while running

--- packages/sdk/src/test/tokenboundClient.test.ts
+++ packages/sdk/src/test/tokenboundClient.test.ts
@@ -5,135 +5,126 @@ import { TEST_CONFIG, TEST_RESULTS } from './config'
 import { ERC_6551_LEGACY_V2 } from '../constants'
 import { TBVersion } from '../types'
 
-describe('Test SDK write methods across standard implementations', () => {
-  runTests({
+describe.each([
+  {
     testName: 'v2',
     expectedTestResults: TEST_RESULTS.V2,
     implementationAddress: ERC_6551_LEGACY_V2.IMPLEMENTATION.ADDRESS,
     registryAddress: ERC_6551_LEGACY_V2.REGISTRY.ADDRESS,
-  })
-  runTests({ testName: 'v3', expectedTestResults: TEST_RESULTS.V3 })
-})
-
-function runTests({
-  testName,
-  expectedTestResults,
-  implementationAddress,
-  registryAddress,
-}: {
-  testName: string
-  expectedTestResults: Object
-  implementationAddress?: `0x${string}`
-  registryAddress?: `0x${string}`
-}) {
-  const tokenboundClient = new TokenboundClient({
-    chainId: TEST_CONFIG.CHAIN_ID,
-    version: testName === 'v2' ? TBVersion.V2 : TBVersion.V3,
-    implementationAddress,
-    registryAddress,
-  })
-
-  test(`tokenboundClient.getAccount ${testName}`, () => {
-    const result = tokenboundClient.getAccount({
-      tokenContract: TEST_CONFIG.TOKEN_CONTRACT,
-      tokenId: TEST_CONFIG.TOKEN_ID,
-    })
-    expect(result).toEqual(expectedTestResults['TB_ACCOUNT'])
-  })
-
-  test.todo('tokenboundClient.getCreationCode')
-
-  if (testName === 'v2') {
-    test(`tokenboundClient.prepareExecuteCall ${testName}`, async () => {
-      const preparedCall = await tokenboundClient.prepareExecuteCall({
-        account: TEST_CONFIG.TB_ACCOUNT,
-        to: TEST_CONFIG.RECIPIENT_ADDRESS,
-        value: 0n,
-        data: TEST_CONFIG.EXAMPLE_DATA,
-      })
-
-      expect(isAddress(preparedCall.to)).toEqual(true)
-      expect(typeof preparedCall.value).toEqual('bigint')
-      expect(isHex(preparedCall.data)).toEqual(true)
-    })
-  }
-  if (testName === 'v3') {
-    test(`tokenboundClient.prepareExecution ${testName}`, async () => {
-      const preparedCall = await tokenboundClient.prepareExecution({
-        account: TEST_CONFIG.TB_ACCOUNT,
-        to: TEST_CONFIG.RECIPIENT_ADDRESS,
-        value: 0n,
-        data: TEST_CONFIG.EXAMPLE_DATA,
-      })
-
-      expect(isAddress(preparedCall.to)).toEqual(true)
-      expect(typeof preparedCall.value).toEqual('bigint')
-      expect(isHex(preparedCall.data)).toEqual(true)
-    })
-  }
-
-  test.todo('tokenboundClient.executeCall')
-
-  test(`tokenboundClient.prepareCreateAccount ${testName}`, async () => {
-    const preparedAccount = await tokenboundClient.prepareCreateAccount({
-      tokenContract: TEST_CONFIG.TOKEN_CONTRACT,
-      tokenId: TEST_CONFIG.TOKEN_ID,
-    })
-
-    expect(isAddress(preparedAccount.to)).toEqual(true)
-    expect(typeof preparedAccount.value).toEqual('bigint')
-    expect(isHex(preparedAccount.data)).toEqual(true)
-  })
-
-  test(`tokenboundClient.checkAccountDeployment ${testName}`, async () => {
-    const isSapienz0Deployed = await tokenboundClient.checkAccountDeployment({
-      accountAddress: TEST_CONFIG.SAPIENZ_GOERLI_TOKEN_TBA_TOKENID_0,
-    })
-    const isSapienz1Deployed = await tokenboundClient.checkAccountDeployment({
-      accountAddress: TEST_CONFIG.SAPIENZ_GOERLI_TOKEN_TBA_TOKENID_1,
-    })
-
-    expect(isSapienz0Deployed).toEqual(true)
-    expect(isSapienz1Deployed).toEqual(false)
-  })
-
-  test(`tokenboundClient.deconstructBytecode ${testName}`, async () => {
-    const bytecode = await tokenboundClient.deconstructBytecode({
-      accountAddress: TEST_CONFIG.SAPIENZ_GOERLI_TOKEN_TBA_TOKENID_0,
-    })
-
-    if (!bytecode) throw new Error('Bytecode is undefined')
-
-    const {
-      chainId,
+  },
+  { testName: 'v3', expectedTestResults: TEST_RESULTS.V3 },
+])(
+  'Test SDK write methods across standard implementations $testName',
+  ({ testName, expectedTestResults, implementationAddress, registryAddress }) => {
+    const tokenboundClient = new TokenboundClient({
+      chainId: TEST_CONFIG.CHAIN_ID,
+      version: testName === 'v2' ? TBVersion.V2 : TBVersion.V3,
       implementationAddress,
-      tokenContract,
-      tokenId,
-      salt,
-      erc1167Header,
-      erc1167Footer,
-    } = bytecode
-
-    expect(chainId).toEqual(TEST_CONFIG.CHAIN_ID)
-    expect(erc1167Header).toEqual(TEST_CONFIG.ERC1167_HEADER)
-    expect(implementationAddress).toEqual(ERC_6551_LEGACY_V2.IMPLEMENTATION.ADDRESS)
-    expect(erc1167Footer).toEqual(TEST_CONFIG.ERC1167_FOOTER)
-    expect(tokenContract).toEqual(TEST_CONFIG.SAPIENZ_GOERLI_CONTRACT)
-    expect(tokenId).toEqual('0')
-    expect(salt).toEqual(0)
-  })
-
-  test(`tokenboundClient.getNFT ${testName}`, async () => {
-    const nft = await tokenboundClient.getNFT({
-      accountAddress: TEST_CONFIG.SAPIENZ_GOERLI_TOKEN_TBA_TOKENID_0,
+      registryAddress,
     })
 
-    if (!nft) throw new Error('Bytecode is undefined')
+    test(`tokenboundClient.getAccount ${testName}`, () => {
+      const result = tokenboundClient.getAccount({
+        tokenContract: TEST_CONFIG.TOKEN_CONTRACT,
+        tokenId: TEST_CONFIG.TOKEN_ID,
+      })
+      expect(result).toEqual(expectedTestResults['TB_ACCOUNT'])
+    })
 
-    const { chainId, tokenContract, tokenId } = nft
+    test.todo('tokenboundClient.getCreationCode')
 
-    expect(chainId).toEqual(TEST_CONFIG.CHAIN_ID)
-    expect(tokenContract).toEqual(TEST_CONFIG.SAPIENZ_GOERLI_CONTRACT)
-    expect(tokenId).toEqual('0')
-  })
-}
+    if (testName === 'v2') {
+      test(`tokenboundClient.prepareExecuteCall ${testName}`, async () => {
+        const preparedCall = await tokenboundClient.prepareExecuteCall({
+          account: TEST_CONFIG.TB_ACCOUNT,
+          to: TEST_CONFIG.RECIPIENT_ADDRESS,
+          value: 0n,
+          data: TEST_CONFIG.EXAMPLE_DATA,
+        })
+
+        expect(isAddress(preparedCall.to)).toEqual(true)
+        expect(typeof preparedCall.value).toEqual('bigint')
+        expect(isHex(preparedCall.data)).toEqual(true)
+      })
+    }
+    if (testName === 'v3') {
+      test(`tokenboundClient.prepareExecution ${testName}`, async () => {
+        const preparedCall = await tokenboundClient.prepareExecution({
+          account: TEST_CONFIG.TB_ACCOUNT,
+          to: TEST_CONFIG.RECIPIENT_ADDRESS,
+          value: 0n,
+          data: TEST_CONFIG.EXAMPLE_DATA,
+        })
+
+        expect(isAddress(preparedCall.to)).toEqual(true)
+        expect(typeof preparedCall.value).toEqual('bigint')
+        expect(isHex(preparedCall.data)).toEqual(true)
+      })
+    }
+
+    test.todo('tokenboundClient.executeCall')
+
+    test(`tokenboundClient.prepareCreateAccount ${testName}`, async () => {
+      const preparedAccount = await tokenboundClient.prepareCreateAccount({
+        tokenContract: TEST_CONFIG.TOKEN_CONTRACT,
+        tokenId: TEST_CONFIG.TOKEN_ID,
+      })
+
+      expect(isAddress(preparedAccount.to)).toEqual(true)
+      expect(typeof preparedAccount.value).toEqual('bigint')
+      expect(isHex(preparedAccount.data)).toEqual(true)
+    })
+
+    test(`tokenboundClient.checkAccountDeployment ${testName}`, async () => {
+      const isSapienz0Deployed = await tokenboundClient.checkAccountDeployment({
+        accountAddress: TEST_CONFIG.SAPIENZ_GOERLI_TOKEN_TBA_TOKENID_0,
+      })
+      const isSapienz1Deployed = await tokenboundClient.checkAccountDeployment({
+        accountAddress: TEST_CONFIG.SAPIENZ_GOERLI_TOKEN_TBA_TOKENID_1,
+      })
+
+      expect(isSapienz0Deployed).toEqual(true)
+      expect(isSapienz1Deployed).toEqual(false)
+    })
+
+    test(`tokenboundClient.deconstructBytecode ${testName}`, async () => {
+      const bytecode = await tokenboundClient.deconstructBytecode({
+        accountAddress: TEST_CONFIG.SAPIENZ_GOERLI_TOKEN_TBA_TOKENID_0,
+      })
+
+      if (!bytecode) throw new Error('Bytecode is undefined')
+
+      const {
+        chainId,
+        implementationAddress,
+        tokenContract,
+        tokenId,
+        salt,
+        erc1167Header,
+        erc1167Footer,
+      } = bytecode
+
+      expect(chainId).toEqual(TEST_CONFIG.CHAIN_ID)
+      expect(erc1167Header).toEqual(TEST_CONFIG.ERC1167_HEADER)
+      expect(implementationAddress).toEqual(ERC_6551_LEGACY_V2.IMPLEMENTATION.ADDRESS)
+      expect(erc1167Footer).toEqual(TEST_CONFIG.ERC1167_FOOTER)
+      expect(tokenContract).toEqual(TEST_CONFIG.SAPIENZ_GOERLI_CONTRACT)
+      expect(tokenId).toEqual('0')
+      expect(salt).toEqual(0)
+    })
+
+    test(`tokenboundClient.getNFT ${testName}`, async () => {
+      const nft = await tokenboundClient.getNFT({
+        accountAddress: TEST_CONFIG.SAPIENZ_GOERLI_TOKEN_TBA_TOKENID_0,
+      })
+
+      if (!nft) throw new Error('Bytecode is undefined')
+
+      const { chainId, tokenContract, tokenId } = nft
+
+      expect(chainId).toEqual(TEST_CONFIG.CHAIN_ID)
+      expect(tokenContract).toEqual(TEST_CONFIG.SAPIENZ_GOERLI_CONTRACT)
+      expect(tokenId).toEqual('0')
+    })
+  }
+)
