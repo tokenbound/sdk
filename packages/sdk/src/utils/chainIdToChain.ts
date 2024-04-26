98ff70b70c272c4143570d6817b5ee5b4d0deb32
BJ Vicks <bj@bjvicks.com>
2024-04-25 19:40:23 -0600

    disable chainIdToChain

--- packages/sdk/src/utils/chainIdToChain.ts
+++ packages/sdk/src/utils/chainIdToChain.ts
@@ -1,41 +1,41 @@
-import { Chain } from 'viem'
-import {
-  mainnet,
-  goerli,
-  polygon,
-  polygonMumbai,
-  sepolia,
-  optimism,
-  arbitrum,
-  optimismGoerli,
-  base,
-  baseGoerli,
-  baseSepolia,
-  lineaTestnet,
-  linea,
-  gnosis,
-  zora,
-  zoraTestnet,
-} from 'viem/chains'
+// import { Chain } from 'viem'
+// import {
+//   mainnet,
+//   goerli,
+//   polygon,
+//   polygonMumbai,
+//   sepolia,
+//   optimism,
+//   arbitrum,
+//   optimismGoerli,
+//   base,
+//   baseGoerli,
+//   baseSepolia,
+//   lineaTestnet,
+//   linea,
+//   gnosis,
+//   zora,
+//   zoraTestnet,
+// } from 'viem/chains'
 
-const enabledChains = {
-  mainnet,
-  goerli,
-  optimismGoerli,
-  polygon,
-  polygonMumbai,
-  sepolia,
-  optimism,
-  arbitrum,
-  base,
-  gnosis,
-  baseGoerli,
-  baseSepolia,
-  lineaTestnet,
-  linea,
-  zora,
-  zoraTestnet,
-}
+// const enabledChains = {
+//   mainnet,
+//   goerli,
+//   optimismGoerli,
+//   polygon,
+//   polygonMumbai,
+//   sepolia,
+//   optimism,
+//   arbitrum,
+//   base,
+//   gnosis,
+//   baseGoerli,
+//   baseSepolia,
+//   lineaTestnet,
+//   linea,
+//   zora,
+//   zoraTestnet,
+// }
 
 /**
  * Gets the chain object for the given chain id.
@@ -43,12 +43,12 @@ const enabledChains = {
  * @returns Viem's chain object.
  */
 
-export function chainIdToChain(chainId: number): Chain {
-  for (const chain of Object.values(enabledChains)) {
-    if (chain.id === chainId) {
-      return chain
-    }
-  }
+// export function chainIdToChain(chainId: number): Chain {
+//   for (const chain of Object.values(enabledChains)) {
+//     if (chain.id === chainId) {
+//       return chain
+//     }
+//   }
 
-  throw new Error(`Chain with id ${chainId} not found`)
-}
+//   throw new Error(`Chain with id ${chainId} not found`)
+// }
